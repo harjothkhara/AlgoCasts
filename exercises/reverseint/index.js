07602c5b6ef007134cfcf419a773c1988d2e1503
harjoth <harjoth.khara@gmail.com>
2020-10-06 13:31:27 -0700

    algocast: reverseInt - initial solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  //  15  981   500   -15   -90
+  //  51  189   005   -51   -09
+  //              5         -9
+  // reverse string variable we will be building
+  let reverse = '';
+  // turn integer into a string
+  const y = n + '';
+  // turn string into an array
+  let arrY = y.split('');
+  // edgecase: add '-' first item in array
+  if (arrY[0] === '-') {
+    // add '-' to the front of variable
+    reverse += arrY[0];
+    // remove '-' from arr we will be iterating through
+    arrY = arrY.slice(1);
+  }
+  // loop through the integer starting from the end
+  for (let i = arrY.length - 1; i >= 0; i--) {
+    // add each integer to variable in reverse order
+    reverse += arrY[i];
+  }
+  return Number(reverse);
+}
 
 module.exports = reverseInt;
